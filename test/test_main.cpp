5fea094c5ad4868568bbb527c84e14ec0a2bc09d
octopuscabbage <octopuscabbage@gmail.com>
2015-10-04 16:43:48 -0500

    added testing facilities

---
+++ test/test_main.cpp
@@ -0,0 +1,10 @@
+#define CATCH_CONFIG_MAIN //Only do this here, include other tests below
+
+#include "catch.hpp"
+
+//Testing framework tutorial provided below
+//https://github.com/philsquared/Catch/blob/master/docs/tutorial.md
+
+TEST_CASE("BLANK TEST","TEST"){
+	REQUIRE( 1 == 1);
+}
